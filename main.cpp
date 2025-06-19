edb8adf0d1d7299a0b9e8e0cee268810db222de5
LiyuanTuo <c17823793046@163.com>
2025-06-19 08:52:06 +0800

    Add NPU_size

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, g[11], k[11], m[11], M, s[501], e[501], cnt[501], latency[11][501], a, b, server_index[501][11], NPU_size[11], request_size[11];
+int N, g[11], k[11], m[11], M, s[501], e[501], cnt[501], latency[11][501], a, b, server_index[501][11], NPU_size[11][11][200000], request_size[11];
 int which_gpu[11]; // which_gpu[i]用于表示第i个服务器应该让哪一个gpu处理传送至服务器i的请求
-// int que[11][11][200000];
 void get_argument_initial()
 {
     cin >> N;
@@ -24,10 +23,15 @@ void get_argument_initial()
     cin >> a >> b;
     for (int i = 1; i <= N; i++)
         which_gpu[i] = 1; // initial which_gpu
+
+    for(int i = 1; i <= N; i++)
+        for(int j = 1; j <= g[i]; j++)
+            for(int k = 0; k <= 200000; k++)
+                NPU_size[i][j][k] = (m[i] - b) / a;// 确实应该向下取整，
+
     for (int i = 1; i <= N; i++)
     {
-        NPU_size[i] = (m[i] - b) / a;             // 确实应该向下取整，
-        request_size[i] = min(NPU_size[i], 1000); // 表示应该向第i个服务器的NPU放入多大的样本数量
+        request_size[i] = min(NPU_size[i][1][1], 1000); // 表示应该向第i个服务器的NPU放入多大的样本数量
     }
 }
 
@@ -46,17 +50,17 @@ void sort_server()
             server_index[i][j] = j; // 存的是服务器的下标
         }
     }
-    cout << endl << "test" << endl;
+    // cout << endl << "test" << endl;
     for (int i = 1; i <= M; i++)
     {
         sort(server_index[i] + 1, server_index[i] + N + 1, [i](int server_index1, int server_index2) -> bool
              { return double(request_time(request_size[server_index1], server_index1, i)) / request_size[server_index1] < double(request_time(request_size[server_index2], server_index2, i)) / request_size[server_index2]; });
-        cout << "user_" << i << ":";
-        for (int j = 1; j <= N; j++)
-        {
-            cout << server_index[i][j] << " ";
-        }
-        cout << endl;
+        // cout << "user_" << i << ":";
+        // for (int j = 1; j <= N; j++)
+        // {
+        //     cout << server_index[i][j] << " ";
+        // }
+        // cout << endl;
     }
     return;
 }
@@ -65,19 +69,19 @@ void solution()
 {
     for (int i = 1; i <= M; i++)
     {
-        int Ti = 0, serverj = i % N + 1;
-        int Bj = min(1000, (m[serverj] - b) / a);
-        Ti = (int)ceil(1.0 * cnt[i] / Bj);
+        int Ti = 0, serverj = server_index[i][1];
+        // Bj = request_size[serverj]
+        Ti = (int)ceil(1.0 * cnt[i] / request_size[serverj]);
         int timej = s[i];
 
         cout << Ti << "\n"; // Ti must less than 300, there won't cost a problem
 
         for (int k = 1; k <= Ti; k++)
         {
-            cout << timej << " " << serverj << " " << which_gpu[serverj] << " " << min(Bj, cnt[i]) << " ";
+            cout << timej << " " << serverj << " " << which_gpu[serverj] << " " << min(request_size[serverj], cnt[i]) << " ";
 
             timej += latency[serverj][i] + 1;
-            cnt[i] -= min(Bj, cnt[i]);
+            cnt[i] -= min(request_size[serverj], cnt[i]);
             which_gpu[serverj] = which_gpu[serverj] % g[serverj] + 1;
         }
         cout << "\n";
